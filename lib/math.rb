10378bf10ee6bc4b9357f36d7117f181d982ffde
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-04 07:17:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -24,4 +24,7 @@ end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  #expect(order_of_operation(7, 43, 23,84)).to eq(18).
+  
+  num1 + ((num2 * num3) / num4)
 end
